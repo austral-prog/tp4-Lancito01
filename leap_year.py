8472c754501fc103ce4d89dc24865196eb24c0b4
Andy <andy@lancito01.dev>
2025-09-12 11:18:21 -0300

    resuelto TP4

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,6 @@
 def leap_year():
-    print("TO DO")
+    y = int(input("Ingrese un año: "))
+    if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
+        print(f"El año {y} es bisiesto")
+    else:
+        print(f"El año {y} no es bisiesto")
--- line.py
+++ line.py
@@ -1,2 +1,20 @@
 def line():
-    print("TO DO")
+    a = float(input("Ingrese el coeficiente A: "))
+    b = float(input("Ingrese el coeficiente B: "))
+    x1 = float(input("Ingrese el coeficiente X1: "))
+    x2 = float(input("Ingrese el coeficiente X2: "))
+
+    print(f"El coeficiente A de su ecuación de la recta es: {a}")
+    print(f"El coeficiente B de su ecuación de la recta es: {b}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {x1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {x2}")
+    print()
+    print(f"Para la siguiente ecuación:\n\tY = {a}X + {b}")
+    print()
+    y1 = a * x1 + b
+    y2 = a * x2 + b
+    print("Dados los siguientes puntos:")
+    print(f"\tP1 ({x1}, {y1})")
+    print(f"\tP2 ({x2}, {y2})")
+    print()
+    print(f"La distancia entre ellos es: {((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5}")
